270fe3f5230eb70ebb33c733a63e22e5ab052ae8
Jaivardhan <jaiv.lal94@gmail.com>
2022-06-26 17:51:16 +0900

    New translations cms.js (Hindi)

--- crwodin/hn/cms.js
+++ crwodin/hn/cms.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'प्रतिक्रिया';
 import CMS from 'netlify-cms-app';
 // Import main site styles as a string to inject into the CMS preview pane
 import styles from '!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css';
